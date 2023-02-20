fbc1c0966bf6f48bc34b4b61f129096847c2634f
Lorenzo Murarotto <lnzmrr@gmail.com>
2023-02-20 11:06:40 +0000

    add example

--- core/src/try_from_field.rs
+++ core/src/try_from_field.rs
@@ -2,8 +2,32 @@ use crate::typed_multipart_error::TypedMultipartError;
 use axum::async_trait;
 use axum::extract::multipart::Field;
 
+/// Types that can be crated from an instance of [Field].
+///
+/// All fields for a given struct must implement this trait to be able to derive
+/// the [TryFromMultipart] trait.
+///
+/// ### Example
+///
+/// ```rust
+/// use axum_typed_multipart::TypedMultipartError;
+/// use axum_typed_multipart::TryFromField;
+/// use axum::async_trait;
+/// use axum::extract::multipart::Field;
+///
+/// struct CoolString(String);
+///
+/// #[async_trait]
+/// impl<'a> TryFromField<'a> for CoolString {
+///     async fn try_from_field(field: Field<'a>) -> Result<Self, TypedMultipartError> {
+///         let text = field.text().await?;
+///         Ok(CoolString(text))
+///     }
+/// }
+/// ```
 #[async_trait]
 pub trait TryFromField<'a>: Sized {
+    /// Consume the input [Field] to create the supplied type.
     async fn try_from_field(field: Field<'a>) -> Result<Self, TypedMultipartError>;
 }
 
