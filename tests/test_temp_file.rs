48c5dca99d5a28576da19988354c6abbbe43a578
Lorenzo Murarotto <lnzmrr@gmail.com>
2023-07-30 16:42:32 +0000

    fix!: remove unnecessary async from tempfile persist

--- tests/test_temp_file.rs
+++ tests/test_temp_file.rs
@@ -4,7 +4,7 @@ use axum_typed_multipart::{TempFile, TryFromMultipart};
 use common_multipart_rfc7578::client::multipart::Form;
 use std::fs::read_to_string;
 use std::io::BufReader;
-use tempfile::tempdir;
+use tempfile::TempDir;
 use util::get_typed_multipart_from_form;
 
 #[derive(TryFromMultipart)]
@@ -25,12 +25,9 @@ async fn test_temp_file() {
 
     let data = get_typed_multipart_from_form::<Foo>(form).await.unwrap().0;
 
-    let temp_dir = tempdir().unwrap();
-    let file_path = temp_dir.path().join("potato.txt");
+    let file_path = TempDir::new().unwrap().into_path().join("potato.txt");
+    data.file.persist(&file_path, false).unwrap();
+    let contents = read_to_string(&file_path).unwrap();
 
-    data.file.persist(&file_path, false).await.unwrap();
-
-    let data = read_to_string(&file_path).unwrap();
-
-    assert_eq!(data, "Potato!");
+    assert_eq!(contents, "Potato!");
 }
