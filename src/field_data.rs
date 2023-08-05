da333daaa79201c09c1e06a4680061bfe5e8428e
Lorenzo Murarotto <lnzmrr@gmail.com>
2023-08-05 09:42:04 +0000

    test: add unit tests

--- src/field_data.rs
+++ src/field_data.rs
@@ -1,6 +1,55 @@
-use crate::{FieldMetadata, TryFromField, TypedMultipartError};
+use crate::{TryFromField, TypedMultipartError};
 use axum::async_trait;
 use axum::extract::multipart::Field;
+use axum::http::HeaderMap;
+
+/// Additional information about the file supplied by the client in the request.
+#[derive(Debug)]
+pub struct FieldMetadata {
+    /// Name of the HTML field in the form.
+    ///
+    /// If the [TryFromMultipart](crate::TryFromMultipart) implementation for
+    /// the struct where this field is used was generated using the derive macro
+    /// it will make it safe to unwrap this value since the field name must
+    /// always be present to allow for mapping it to a struct field.
+    ///
+    /// Extracted from the
+    /// [`Content-Disposition`](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition)
+    /// header.
+    pub name: Option<String>,
+
+    /// Original name of the file transmitted.
+    ///
+    /// The filename is always optional and must not be used blindly by the
+    /// application: path information should be stripped, and conversion to the
+    /// server file system rules should be done.
+    ///
+    /// Extracted from the
+    /// [`Content-Disposition`](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition)
+    /// header.
+    pub file_name: Option<String>,
+
+    /// MIME type of the field.
+    ///
+    /// Extracted from the
+    /// [`Content-Type`](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type)
+    /// header.
+    pub content_type: Option<String>,
+
+    /// HTTP headers sent with the field.
+    pub headers: HeaderMap,
+}
+
+impl From<&Field<'_>> for FieldMetadata {
+    fn from(field: &Field) -> Self {
+        Self {
+            name: field.name().map(String::from),
+            file_name: field.file_name().map(String::from),
+            content_type: field.content_type().map(String::from),
+            headers: field.headers().clone(),
+        }
+    }
+}
 
 /// Wrapper struct that allows to retrieve both the field contents and the
 /// additional metadata provided by the client.
@@ -14,11 +63,24 @@ use axum::extract::multipart::Field;
 /// ## Example
 ///
 /// ```rust
-/// use axum_typed_multipart::{FieldData, TryFromMultipart};
+/// use axum::http::StatusCode;
+/// use axum::routing::post;
+/// use axum::Router;
+/// use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
 ///
 /// #[derive(TryFromMultipart)]
 /// struct FileUpload {
 ///     input_file: FieldData<String>,
+/// }
+///
+/// async fn handler(
+///     TypedMultipart(FileUpload { input_file }): TypedMultipart<FileUpload>,
+/// ) -> StatusCode {
+///     println!("Field name: {}", input_file.metadata.name.unwrap());
+///     println!("File name: {}", input_file.metadata.file_name.unwrap());
+///     println!("Content type: {}", input_file.metadata.content_type.unwrap());
+///     println!("Body: {}", input_file.contents);
+///     StatusCode::OK
 /// }
 /// ```
 #[derive(Debug)]
@@ -36,5 +98,35 @@ impl<T: TryFromField> TryFromField for FieldData<T> {
         let metadata = FieldMetadata::from(&field);
         let contents = T::try_from_field(field, limit_bytes).await?;
         Ok(Self { metadata, contents })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::extract::Multipart;
+    use axum::routing::post;
+    use axum::Router;
+    use axum_test_helper::TestClient;
+    use reqwest::multipart::{Form, Part};
+
+    async fn handler(mut multipart: Multipart) -> Result<(), TypedMultipartError> {
+        let field = multipart.next_field().await?.unwrap();
+        let field_data = FieldData::<String>::try_from_field(field, None).await?;
+
+        assert_eq!(field_data.metadata.name.unwrap(), "input_file");
+        assert_eq!(field_data.metadata.file_name.unwrap(), "test.txt");
+        assert_eq!(field_data.metadata.content_type.unwrap(), "text/plain");
+        assert_eq!(field_data.contents, "test");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_field_data() {
+        let part = Part::text("test").file_name("test.txt").mime_str("text/plain").unwrap();
+        let form = { Form::new().part("input_file", part) };
+        let client = TestClient::new(Router::new().route("/", post(handler)));
+        client.post("/").multipart(form).send().await;
     }
 }
