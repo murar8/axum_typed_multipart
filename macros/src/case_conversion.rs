4f1fd271b376ed706933d8e7603a90033d315161
Lorenzo Murarotto <lnzmrr@gmail.com>
2023-08-15 13:07:59 +0000

    test: increase coverage

--- macros/src/case_conversion.rs
+++ macros/src/case_conversion.rs
@@ -67,7 +67,7 @@ impl<'a> TryFrom<&'a str> for RenameCase {
 
 #[cfg(test)]
 mod tests {
-    use super::RenameCase;
+    use super::*;
 
     fn test_helper<const N: usize>(case: RenameCase, should_be: &str, variants: [&str; N]) {
         for variant in variants {
@@ -129,6 +129,32 @@ mod tests {
             RenameCase::Camel,
             "camelCase",
             ["camelCase", "camel_case", "camel-case", "CamelCase"],
+        );
+    }
+
+    #[test]
+    fn test_try_from_valid() {
+        assert_eq!(RenameCase::try_from("snake_case").unwrap(), RenameCase::Snake);
+        assert_eq!(RenameCase::try_from("kebab-case").unwrap(), RenameCase::Kebab);
+        assert_eq!(RenameCase::try_from("camelCase").unwrap(), RenameCase::Camel);
+        assert_eq!(RenameCase::try_from("PascalCase").unwrap(), RenameCase::Pascal);
+        assert_eq!(RenameCase::try_from("UPPERCASE").unwrap(), RenameCase::Upper);
+        assert_eq!(RenameCase::try_from("lowercase").unwrap(), RenameCase::Lower);
+    }
+
+    #[test]
+    fn test_try_from_err() {
+        let error = RenameCase::try_from("invalid_case").unwrap_err();
+        assert!(matches!(error, InvalidCase));
+        assert_eq!(error.to_string(), "invalid case conversion option");
+    }
+
+    #[test]
+    fn test_from_option_fallible() {
+        assert_eq!(RenameCase::from_option_fallible(0, None::<String>), None);
+        assert_eq!(
+            RenameCase::from_option_fallible("", Some("snake_case")),
+            Some(RenameCase::Snake)
         );
     }
 }
