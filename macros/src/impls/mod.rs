29286f9ec504a40f9de974f15686ef59f67086d5
Lorenzo Murarotto <lnzmrr@gmail.com>
2025-12-26 15:55:24 +0100

    feat: add internal MultipartBuilder trait and derive macro

--- macros/src/impls/mod.rs
+++ macros/src/impls/mod.rs
@@ -1,5 +1,6 @@
 // See: https://github.com/cloudflare/foundations/issues/50
 #![allow(clippy::manual_unwrap_or_default)]
 
+pub mod multipart_builder;
 pub mod try_from_field;
 pub mod try_from_multipart;
