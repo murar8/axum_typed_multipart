a7d41523cb42152605ecd67ad997943ba134069b
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-01-01 13:38:56 +0100

    refactor: simplify pub(crate) to pub in private module

--- macros/src/derive_input.rs
+++ macros/src/derive_input.rs
@@ -5,50 +5,50 @@ use quote::{quote, ToTokens};
 
 #[derive(Debug, FromDeriveInput)]
 #[darling(attributes(try_from_multipart), supports(struct_named))]
-pub(crate) struct InputData {
-    pub(crate) ident: syn::Ident,
+pub struct InputData {
+    pub ident: syn::Ident,
 
-    pub(crate) data: darling::ast::Data<(), FieldData>,
-
-    #[darling(default)]
-    pub(crate) strict: bool,
+    pub data: darling::ast::Data<(), FieldData>,
 
     #[darling(default)]
-    pub(crate) rename_all: Option<RenameCase>,
+    pub strict: bool,
 
     #[darling(default)]
-    pub(crate) state: Option<syn::Path>,
+    pub rename_all: Option<RenameCase>,
+
+    #[darling(default)]
+    pub state: Option<syn::Path>,
 }
 
 impl InputData {
-    pub(crate) fn builder_ident(&self) -> syn::Ident {
+    pub fn builder_ident(&self) -> syn::Ident {
         crate::impls::multipart_builder::builder_ident(&self.ident)
     }
 
-    pub(crate) fn generic(&self) -> Option<impl ToTokens> {
+    pub fn generic(&self) -> Option<impl ToTokens> {
         self.state.is_none().then(|| quote! { <S: Sync> })
     }
 
-    pub(crate) fn state_ty(&self) -> impl ToTokens {
+    pub fn state_ty(&self) -> impl ToTokens {
         self.state.as_ref().map(|s| quote! { #s }).unwrap_or(quote! { S })
     }
 }
 
 #[derive(Debug, FromField)]
 #[darling(attributes(form_data))]
-pub(crate) struct FieldData {
-    pub(crate) ident: Option<syn::Ident>,
+pub struct FieldData {
+    pub ident: Option<syn::Ident>,
 
-    pub(crate) ty: syn::Type,
+    pub ty: syn::Type,
 
-    pub(crate) field_name: Option<String>,
-
-    #[darling(default)]
-    pub(crate) limit: LimitBytes,
+    pub field_name: Option<String>,
 
     #[darling(default)]
-    pub(crate) default: bool,
+    pub limit: LimitBytes,
 
     #[darling(default)]
-    pub(crate) nested: bool,
+    pub default: bool,
+
+    #[darling(default)]
+    pub nested: bool,
 }
