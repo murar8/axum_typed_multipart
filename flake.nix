219fca13856ca5afea9f2726ad78d1250355db9e
Lorenzo Murarotto <lnzmrr@gmail.com>
2026-05-24 11:18:42 +0200

    chore: add nix flake dev environment
    Adds flake.nix, flake.lock, and rust-toolchain.toml for a reproducible
    Rust dev shell via fenix. Bumps commitlint and markdownlint-cli
    pre-commit hooks, and works around a taplo schemastore fetch issue.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  # Based on https://github.com/the-nix-way/dev-templates/blob/4eab4b7c62077d14773edc6abcb4bf7664bdcc1f/rust/flake.nix
+  description = "Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs-unstable
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    {
+      overlays.default = final: prev: {
+        rustToolchain = fenix.packages.${prev.stdenv.hostPlatform.system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
+        };
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        formatter = pkgs.nixfmt;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            nixfmt
+            statix
+            pre-commit
+          ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+          # Required at test/runtime: openssl-sys links libssl.so.3 dynamically.
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:''${LD_LIBRARY_PATH:-}
+          '';
+        };
+      }
+    );
+}
